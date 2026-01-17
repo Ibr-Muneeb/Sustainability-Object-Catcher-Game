4b596a64980dff04f894aa5faa0a2cfae6ce58a4
Ibr-Muneeb <ibrahimmuneeb22@gmail.com>
2026-01-17 12:23:55 -0500

    Added movement controls to the catcher

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    var pane = $('#pane'),
+        box = $('#catcher'),
+        w = pane.width() - box.width(),
+        d = {},
+        x = 5;
+
+    function newv(v, a, b) {
+        var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
+        return n < 0 ? 0 : n > w ? w : n;
+    }
+
+    $(window).keydown(function (e) {
+        d[e.which] = true;
+    });
+
+    $(window).keyup(function (e) {
+        d[e.which] = false;
+    });
+
+    setInterval(function () {
+        box.css({
+            left: function (i, v) {
+                return newv(v, 37, 39);
+            }
+        });
+    }, 20);
+
+});
